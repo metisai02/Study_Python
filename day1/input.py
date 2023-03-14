5d55097d06b78aafc83ef7ade1a1866234396830
nga <nga.leddt02@hcmut.edu.vn>
2023-03-15 02:46:34 +0700

    day1

---
+++ day1/input.py
@@ -0,0 +1,11 @@
+
+# a = input("a: ")
+# b = input("b: ")
+
+# print("b = "+a)
+print("wellcome to the first test in a day")
+print("what the fuk are you ?")
+name = input()
+print("how long is ur dick")
+length = input()
+print("hello " + name + ". length of ur dick " + length + ". Thats so awesome")
