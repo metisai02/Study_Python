468ce911fb99b4874bf4b1cb5c7cd2395fdb8bcc
nga <nga.leddt02@hcmut.edu.vn>
2023-03-15 02:46:56 +0700

    day3

---
+++ day3/final.py
@@ -0,0 +1,38 @@
+print (''''
+   .
+  /:\
+  |:|
+  |:|
+  |:|                              *_   _   _   _   _   _   _   *
+  |:|      __                      | `_' `-' `_' `-' `_' `-' `_'|
+,_|:|_,   /  )                     |                            |
+  (Oo    / _I_                     |  LONG LIVE  alt.ascii-art  |
+   +\ \  || __|            ^       |                            |       ^
+      \ \||___|            |       |     Free & Unmoderated     |       |
+        \ /.:.\-\          |  (*)  |_   _   _   _   _   _   _   |  \^/  |
+         |.:. /-----\      | _<">_ | `_' `-' `_' `-' `_' `-' `_'| _(#)_ |
+         |___|  oOo  |    o+o \a/ \0                            0/ \a/ (=)
+         /   ||     0'\a-a/\/                            \/\a-a/`0
+        |_____\  :  /        /_^_\ |                            | /_^_\
+         | |  \ \:/          || || |                            | || ||
+         | |   | |           d|_|b_T____________________________T_d|_|b
+         \ /   | \___              /                           /
+         / |   \_____\           /                            /
+         `-'                   /                             /  -cfbd-
+____________________________ /                             /__________
+''')
+
+
+print("weocome to treasure Island")
+if input("Your mission to find the treasure. left or right\n ") == "right":
+    print("Game over")
+else:
+    if input("swim or wait\n") == "swim":
+        print("Game over")
+    else:
+        chose = input("which door? Red, blue or yellow\n")
+        if chose =="red" and chose == blue:
+            
+            print("Game over")
+        else:
+            print("You win")
