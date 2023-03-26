6b4c2d18e9cd4d4d23d80b77b577fc82e96f487d
nga <nga.leddt02@hcmut.edu.vn>
2023-03-26 14:14:39 +0700

    day18

--- Intermediate/day18/main.py
+++ Intermediate/day18/main.py
@@ -1,6 +1,6 @@
 import colorgram
 
-colors = colorgram.extract('Python\Python_WorkSpace\Intermediate\day18\shi.jpg', 6)
+colors = colorgram.extract('shi.jpg', 6)
 first_color = colors[0]
 rgb = first_color.rgb
 
